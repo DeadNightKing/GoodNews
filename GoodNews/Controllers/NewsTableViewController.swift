40b3bb2113cfdd0e665090e83e3fd87faefb2731
RyanChou <ryanchou@welltend.com.tw>
2022-07-28 13:53:59 +0800

    Setting UI

---
+++ GoodNews/Controllers/NewsTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewsTableViewController.swift
+//  GoodNews
+//
+//  Created by Flow_RyanChou on 2022/7/28.
+//
+
+import Foundation
+import UIKit
+
+class NewsTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
